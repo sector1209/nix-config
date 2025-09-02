a5a612136503e60ab3f64a278b8e9db83bd491c1
Sector1209 <gh@danmail.me>
2025-09-02 11:47:10 +0100

    reorganised nixosRoles structure

---
+++ nixosRoles/services/default.nix
@@ -0,0 +1,28 @@
+{
+  ...
+}:
+{
+
+  imports = [
+    ./acme.nix
+    ./beszel.nix
+    ./borgbackupServer.nix
+    ./docker.nix
+    ./flakeAutoUpdateSvr.nix
+    ./glances.nix
+    ./gotify.nix
+    ./grafanaStack.nix
+    ./immich.nix
+    ./influxdb.nix
+    ./jellyfin.nix
+    ./nextcloud.nix
+    ./nginx.nix
+    ./prometheus-exporter.nix
+    ./promtail.nix
+    ./ssh.nix
+    ./tailscale.nix
+    ./technitium-dns.nix
+    ./vscode-server.nix
+  ];
+
+}
