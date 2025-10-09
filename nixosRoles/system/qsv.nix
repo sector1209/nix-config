159cc64aa5b1e7d2436b1f8139272c9b6edcc9e0
Sector1209 <gh@danmail.me>
2025-10-09 17:35:38 +0100

    Clean up QSV role

--- nixosRoles/system/qsv.nix
+++ nixosRoles/system/qsv.nix
@@ -12,11 +12,6 @@
 
   config = lib.mkIf config.roles.qsv.enable {
 
-    # enable vaapi on OS-level
-    #    nixpkgs.config.packageOverrides = pkgs: {
-    #      vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
-    #    };
-
     systemd.services.jellyfin.environment.LIBVA_DRIVER_NAME = "iHD"; # Or "i965" if using older driver
 
     environment.sessionVariables = {
@@ -26,22 +21,15 @@
     hardware.graphics = {
       enable = true;
       extraPackages = with pkgs; [
-        intel-media-driver # For Broadwell (2014) or newer processors. LIBVA_DRIVER_NAME=iHD
-        #        intel-vaapi-driver # previously vaapiIntel
-        #       vaapiVdpau
-        libva-vdpau-driver # Previously vaapiVdpau
-        #        libvdpau-va-gl
-        intel-compute-runtime-legacy1
-        #        intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
-        #        vpl-gpu-rt # QSV on 11th gen or newer
-        #        intel-media-sdk # QSV up to 11th gen
+        intel-media-driver # The core QuickSync driver. VA-API support for Broadwell (2014) or newer processors. LIBVA_DRIVER_NAME=iHD
+        intel-compute-runtime-legacy1 # OpenCL filter support for Gen8,9,11 GPUs (hardware tonemapping and subtitle burn-in)
         intel-ocl # OpenCL support
       ];
     };
 
     nixpkgs.config.allowUnfree = true;
 
-    # map the render and video groups to the gids of the proxmox host
+    # Map the render and video groups to the GIDs on the Proxmox host
     users.groups = {
       render.gid = lib.mkForce 104;
       video.gid = lib.mkForce 44;
