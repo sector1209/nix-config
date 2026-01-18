e8ec56b445e16776a711ba28bee875a0a4055bc3
Sector1209 <gh@danmail.me>
2026-01-18 19:38:46 +0000

    Move default authorized public keys to secrets specialArg

--- nixosRoles/system/users.nix
+++ nixosRoles/system/users.nix
@@ -25,13 +25,7 @@ in
         isNormalUser = lib.mkDefault true;
         initialHashedPassword = lib.mkDefault secrets.default-initialHashedPassword;
         extraGroups = lib.mkDefault [ "wheel" ];
-        openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIInQa//eQdA7R3LWyccy3dyNqbjisirWiCaeymYc2C7l dan@danbook-pro.bee-atria.ts.net"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKXG07Mrar3L5LtjVQiO26Rs00lx4073dYGPMo3gcwVf danplus_id_ed25519"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF7iLoXoXsChx1KHnxh5IDCgnnRoLov6vl7fe6RqWdlB patrik_id_ed25519.pub"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL91ufGabBT6wyThCZ8prx/bcWh3+aeTeswp14sJNJd2 dan@nixos-testing"
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILWbcgTPKaYp2JYNF/Dn/hD9NKZCGk6knd2e1mgZeaMU dan@ferb"
-        ];
+        openssh.authorizedKeys.keys = secrets.authorizedKeys-list;
       };
     };
 
