6572f3c30705545fb423cfdcc4f62a208b0597e2
Sector1209 <gh@danmail.me>
2026-06-20 19:30:45 +0100

    Bump nixpkgs stable to 26.05

--- flake.nix
+++ flake.nix
@@ -4,12 +4,12 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Home manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -23,7 +23,7 @@
 
     # Nixvim
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-25.11";
+      url = "github:nix-community/nixvim/nixos-26.05";
       #      inputs.nixpkgs.follows = "nixpkgs";
     };
 
