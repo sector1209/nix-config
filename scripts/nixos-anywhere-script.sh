9b676ca7a904c8b7b09653bdaf767e091522c059
Sector1209 <gh@danmail.me>
2026-06-22 18:22:30 +0100

    Change nixos-anywhere script to save secrets at /persist

--- scripts/nixos-anywhere-script.sh
+++ scripts/nixos-anywhere-script.sh
@@ -14,13 +14,13 @@ cleanup() {
 trap cleanup EXIT
 
 # Create the directory where sshd expects to find the host keys
-install -d -m755 "$temp/persist/system/sops-keys/sops/age"
+install -d -m755 "$temp/persist/sops-keys/sops/age"
 
 # Decrypt your private key from the password store and copy it to the temporary directory
-cat /sops-keys/sops/age/keys.txt > "$temp/persist/system/sops-keys/sops/age/keys.txt"
+cat /sops-keys/sops/age/keys.txt > "$temp/persist/sops-keys/sops/age/keys.txt"
 
 # Set the correct permissions so sshd will accept the key
-chmod 600 "$temp/persist/system/sops-keys/sops/age/keys.txt"
+chmod 600 "$temp/persist/sops-keys/sops/age/keys.txt"
 
 # Install NixOS to the host system with our secrets
 nixos-anywhere --extra-files "$temp" --flake .#$installhost --target-host $targethost $sshopts
