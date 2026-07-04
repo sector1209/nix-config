a7cea26636ab35ec790126fa24d97513d50c42e6
Sector1209 <gh@danmail.me>
2026-07-04 01:10:10 +0100

    Make host ssh config and private keys dependent on roles.deployMachine.devUser

--- homeManagerRoles/programs/ssh.nix
+++ homeManagerRoles/programs/ssh.nix
@@ -19,12 +19,20 @@ in
 
     roles.sops.enable = true;
 
-    sops.secrets."keys/nixos-dan-key" = {
-      mode = "0600";
-    };
-    sops.secrets."keys/nixos-deploy-key" = {
-      mode = "0600";
-    };
+    sops.secrets =
+      lib.mkIf
+        (
+          osConfig.roles.deployMachine.enable
+          && (osConfig.roles.deployMachine.devUser == config.home.username)
+        )
+        {
+          "keys/nixos-dan-key" = {
+            mode = "0600";
+          };
+          "keys/nixos-deploy-key" = {
+            mode = "0600";
+          };
+        };
 
     # Configure authentication for git repos
     programs.ssh = {
@@ -45,29 +53,35 @@ in
           ControlPersist = "no";
         };
       }
-      // lib.optionalAttrs osConfig.roles.deployMachine.enable {
-        "dan" = lib.hm.dag.entryBefore [ "deploy" ] {
-          header = "Match user dan";
-          HostName = "%h";
-          User = "dan";
-          IdentityFile = [
-            config.sops.secrets."keys/nixos-dan-key".path
-          ];
-          PreferredAuthentications = "publickey";
-          AddKeysToAgent = "yes";
-        };
-        "deploy" = lib.hm.dag.entryAnywhere {
-          header = "Match user deploy";
-          HostName = "%h";
-          User = "deploy";
-          IdentityFile = [
-            config.sops.secrets."keys/nixos-deploy-key".path
-          ];
-          PreferredAuthentications = "publickey";
-          AddKeysToAgent = "yes";
-          ForwardAgent = "yes";
-        };
-      };
+      //
+        lib.optionalAttrs
+          (
+            osConfig.roles.deployMachine.enable
+            && (osConfig.roles.deployMachine.devUser == config.home.username)
+          )
+          {
+            "dan" = lib.hm.dag.entryBefore [ "deploy" ] {
+              header = "Match user dan";
+              HostName = "%h";
+              User = "dan";
+              IdentityFile = [
+                config.sops.secrets."keys/nixos-dan-key".path
+              ];
+              PreferredAuthentications = "publickey";
+              AddKeysToAgent = "yes";
+            };
+            "deploy" = lib.hm.dag.entryAnywhere {
+              header = "Match user deploy";
+              HostName = "%h";
+              User = "deploy";
+              IdentityFile = [
+                config.sops.secrets."keys/nixos-deploy-key".path
+              ];
+              PreferredAuthentications = "publickey";
+              AddKeysToAgent = "yes";
+              ForwardAgent = "yes";
+            };
+          };
     };
 
   };
