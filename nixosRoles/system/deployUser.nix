6ed48331afeb61c0b98953899dbcf4377749a490
Sector1209 <gh@danmail.me>
2026-07-06 22:33:56 +0100

    Limit deploy user sudo to commands needed for remote rebuilding

--- nixosRoles/system/deployUser.nix
+++ nixosRoles/system/deployUser.nix
@@ -29,8 +29,6 @@ in
       isSystemUser = true;
       shell = pkgs.bash;
       group = "deploy";
-      # Add to wheel group for sudo access
-      extraGroups = [ "wheel" ];
       # Disable password login
       hashedPassword = null;
       # Needed for SSH agent forwarding
@@ -45,17 +43,42 @@ in
     # Disable passwordless sudo generally
     security.sudo.wheelNeedsPassword = lib.mkForce true;
 
+    # Limit sudo access to commands needed for remote rebuilding
+    security.sudo.extraRules =
+      let
+        storePrefix = "/nix/store/*";
+        systemName = "nixos-system-${config.networking.hostName}-*";
+        envVariants = [
+          "/usr/bin/env"
+          "env"
+        ];
+      in
+      [
+        {
+          users = [ "deploy" ];
+          runAs = "root";
+          commands =
+            (map (env: {
+              command = ''/bin/sh -c exec ${env} -i PATH\="''${PATH-}" "$@" sh nix-env -p /nix/var/nix/profiles/system --set ${storePrefix}-${systemName}'';
+            }) envVariants)
+            ++ (map (env: {
+              command = ''/bin/sh -c exec ${env} -i PATH\="''${PATH-}" LOCALE_ARCHIVE\="''${LOCALE_ARCHIVE-}" NIXOS_NO_CHECK\="''${NIXOS_NO_CHECK-}" NIXOS_INSTALL_BOOTLOADER\=[01] "$@" sh systemd-run -E LOCALE_ARCHIVE -E NIXOS_INSTALL_BOOTLOADER -E NIXOS_NO_CHECK --collect --no-ask-password --pipe --quiet --service-type\=exec --unit\=nixos-rebuild-switch-to-configuration ${storePrefix}-${systemName}/bin/switch-to-configuration *'';
+            }) envVariants);
+        }
+      ];
+
     programs.ssh.startAgent = true;
 
-    # Enable PAM authentication with SSH keys
+    # Enable pam authentication with SSH keys
     security.pam = {
-      services.sudo = {
-        sshAgentAuth = true;
-        logFailures = true;
-      };
       sshAgentAuth = {
         enable = true;
         authorizedKeysFiles = [ "/etc/ssh/authorized_keys.d/%u" ];
+      };
+      # Let pam authenticate sudo via SSH keys
+      services.sudo = {
+        sshAgentAuth = true;
+        logFailures = true;
       };
     };
 
